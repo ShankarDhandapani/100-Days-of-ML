df98df987844fc3f2736aca7d3efac74de4694f3
ShankarDhandapani <shankardhandapani1@gmail.com>
2020-08-09 21:42:07 +0530

    Day 19

---
+++ Day019/routes.py
@@ -0,0 +1,65 @@
+from flask import render_template, url_for, flash, redirect, request
+from Day019.models import User, Post
+from Day019.form import RegistrationForm, LoginForm
+from Day019 import app, db, bcrypt
+from flask_login import login_user, current_user, logout_user, login_required
+
+posts = [
+    {
+        'author' : 'Shankar Dhandapani',
+        'title' : 'Portfolio',
+        'content' : "I'm Shankar. I'm an freelancer and Developer.",
+        'date_posted' : 'April 20, 2018'
+    },
+    {
+        'author' : 'Shankar Dhandapani',
+        'title' : 'My Blog 2',
+        'content' : "Second Blog content",
+        'date_posted' : 'April 20, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html', posts=posts, title="Home Page")
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About Page")    
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Failed. Please check you credentials.', 'danger')
+    return render_template('login.html', title="Login Page", form=form)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Hi {form.username.data}, you're account is ready to use.", 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title="Register Page", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title="Account")
